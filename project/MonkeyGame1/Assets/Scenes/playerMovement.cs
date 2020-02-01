4cad5a7a0470d61f473a7199fcc81f614db2a21f
Mark Pedroso <pedrosomark98@gmail.com>
2020-02-01 03:22:50 -0500

    Basic movement demo

---
+++ project/MonkeyGame1/Assets/Scenes/playerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        speed = 10.0f;
+    }
+
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+    }
+}
