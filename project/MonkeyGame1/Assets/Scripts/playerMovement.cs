51bbceb99c16ef04e1e9e43259c1f4912f39c9dd
Mark Pedroso <pedrosomark98@gmail.com>
2020-02-23 18:11:33 -0500

    redid movement script to make it more testable

--- project/MonkeyGame1/Assets/Scripts/playerMovement.cs
+++ project/MonkeyGame1/Assets/Scripts/playerMovement.cs
@@ -4,32 +4,36 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public float speed;
-
-    void Start()
+    private const float normalSpeed = 10f;
+    private void FixedUpdate()
     {
-        speed = 10.0f;
+        if (Input.GetButton("Horizontal"))
+            MoveHorizontally(Input.GetAxis("Horizontal"));
+        if (Input.GetButton("Vertical"))
+            MoveVertically(Input.GetAxis("Vertical"));
     }
 
-    void OnCollisionEnter (Collision collision)
- 	{
-     if (collision.gameObject.tag == "Wall")
-     {
-         Debug.Log("COLLISION");
-     }
- 	}
-
-    void Update()
+    private void MoveHorizontally(float value)
     {
-        transform.Translate(
-        	Input.GetAxis("Horizontal") *
-        		Time.deltaTime *
-        		speed, 
-      		0f, 
-      		Input.GetAxis("Vertical") *
-      			Time.deltaTime *
-      			speed
-      	);
+        float deltaX = Time.fixedDeltaTime * value * normalSpeed;
+        TransformPosition(deltaX, 0f);
+    }
 
+    private void MoveVertically(float value)
+    {
+        float deltaY = Time.fixedDeltaTime * value * normalSpeed;
+        TransformPosition(0f, deltaY);
+    }
+
+    private void TransformPosition(float deltaX, float deltaY)
+    {
+        transform.Translate(deltaX, 0f, deltaY);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            Debug.Log("COLLISION");
+        }
     }
 }
