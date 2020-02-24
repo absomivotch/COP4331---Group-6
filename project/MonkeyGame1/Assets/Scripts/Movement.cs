c7338d4b59b4b7ad84572ce66d4b7aa764bd5d23
Mark Pedroso <pedrosomark98@gmail.com>
2020-02-23 22:00:33 -0500

    Added movement unit test

---
+++ project/MonkeyGame1/Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement
+{
+    public float Speed;
+
+    public Movement(float speed)
+    {
+        Speed = speed;
+    }
+
+    public Vector3 Calculate(float h, float v, float deltaTime)
+    {
+        var x = h * Speed * deltaTime;
+        var z = v * Speed * deltaTime;
+
+        return new Vector3(-x, 0, -z);
+    }
+}
--- project/MonkeyGame1/Assets/Scripts/playerMovement.cs
+++ project/MonkeyGame1/Assets/Scripts/playerMovement.cs
@@ -4,36 +4,18 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    private const float normalSpeed = 10f;
-    private void FixedUpdate()
+    public float Speed = 10;
+    public Movement Movement;
+    private void Start()
     {
-        if (Input.GetButton("Horizontal"))
-            MoveHorizontally(Input.GetAxis("Horizontal"));
-        if (Input.GetButton("Vertical"))
-            MoveVertically(Input.GetAxis("Vertical"));
+        Movement = new Movement(Speed);
     }
 
-    private void MoveHorizontally(float value)
+    private void Update()
     {
-        float deltaX = Time.fixedDeltaTime * value * normalSpeed;
-        TransformPosition(deltaX, 0f);
-    }
-
-    private void MoveVertically(float value)
-    {
-        float deltaY = Time.fixedDeltaTime * value * normalSpeed;
-        TransformPosition(0f, deltaY);
-    }
-
-    private void TransformPosition(float deltaX, float deltaY)
-    {
-        transform.Translate(-deltaX, 0f, -deltaY);
-    }
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Wall")
-        {
-            Debug.Log("COLLISION");
-        }
+        transform.position += Movement.Calculate(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical"),
+            Time.deltaTime);
     }
 }
