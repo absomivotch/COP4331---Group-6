3a8fffa8a1adb541516a9e22e362f30e1dc865e3
cn7abc <33703959+cn7abc@users.noreply.github.com>
2020-03-01 16:36:22 -0500

    added default cursor & glowing cursor when selecting the players or a button

---
+++ project/MonkeyGame1/Assets/Scripts/cursorScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursorScript: MonoBehaviour
+{
+
+    public Texture2D cursorArrow;
+    public Texture2D cursorGlow;
+
+    void Start()
+    {
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    void OnMouseEnter(){
+        Cursor.SetCursor(cursorGlow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    void OnMouseExit(){
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+}
