b075b1e51a6f1ffe7a24408517f36b46fe0a252f
cn7abc <33703959+cn7abc@users.noreply.github.com>
2020-03-08 20:30:31 -0400

    added player selection thru maycasting script

---
+++ project/MonkeyGame1/Assets/Scripts/playerSelect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class playerSelect : MonoBehaviour
+{
+    public float rayLength;
+    public LayerMask layermask;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()){
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, rayLength, layermask)){
+                Debug.Log(hit.collider.name);
+            }
+        }
+    }
+}
