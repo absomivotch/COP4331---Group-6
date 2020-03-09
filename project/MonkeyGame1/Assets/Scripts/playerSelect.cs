ae9b26716b26301751930c0c957a75618ca592b6
cn7abc <33703959+cn7abc@users.noreply.github.com>
2020-03-08 20:51:37 -0400

    movememnt thru cursor done.

--- project/MonkeyGame1/Assets/Scripts/playerSelect.cs
+++ project/MonkeyGame1/Assets/Scripts/playerSelect.cs
@@ -8,13 +8,38 @@ public class playerSelect : MonoBehaviour
     public float rayLength;
     public LayerMask layermask;
 
+     public GameObject playerA, playerB, playerC;
+
+     void Start()
+    {
+        playerA.GetComponent<playerMovement>().enabled = false;
+        playerB.GetComponent<playerMovement>().enabled = false;
+        playerC.GetComponent<playerMovement>().enabled = false;
+    }
+
+
     void Update()
     {
         if(Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()){
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if(Physics.Raycast(ray, out hit, rayLength, layermask)){
-                Debug.Log(hit.collider.name);
+               Debug.Log(hit.collider.name); 
+               if(hit.collider.name == "player1SlotA"){
+                    playerA.GetComponent<playerMovement>().enabled = true;
+                    playerB.GetComponent<playerMovement>().enabled = false;
+                    playerC.GetComponent<playerMovement>().enabled = false;
+               }
+               else if(hit.collider.name == "player1SlotB"){
+                    playerA.GetComponent<playerMovement>().enabled = false;
+                    playerB.GetComponent<playerMovement>().enabled = true;
+                    playerC.GetComponent<playerMovement>().enabled = false;
+               }
+               else if(hit.collider.name == "player1SlotC"){
+                    playerA.GetComponent<playerMovement>().enabled = false;
+                    playerB.GetComponent<playerMovement>().enabled = false;
+                    playerC.GetComponent<playerMovement>().enabled = true;
+               }
             }
         }
     }
