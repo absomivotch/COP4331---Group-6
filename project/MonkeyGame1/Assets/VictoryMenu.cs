fb9f6afa9822f46290acd4fe42804c31e6329f31
Mark Pedroso <pedrosomark98@gmail.com>
2020-04-10 00:39:54 -0400

    Added victory screen and updated main menu

---
+++ project/MonkeyGame1/Assets/VictoryMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryMenu : MonoBehaviour
+{
+    public void VicPlayAgain()
+    {
+        SceneManager.LoadScene("Level0");
+    }
+    public void VicLoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void VicQuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
